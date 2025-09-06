07552a8921f1f7f7e07a798913612a1c6db030e1
skydrying <ruvasikfarm1@mail.ru>
2025-09-06 17:54:56 +0700

    replace gitignore

--- src/main/java/lisovskiy/studentslab/repository/UsersRepository.java
+++ src/main/java/lisovskiy/studentslab/repository/UsersRepository.java
@@ -1,11 +1,10 @@
 package lisovskiy.studentslab.repository;
 
-
 import lisovskiy.studentslab.entity.Users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UsersRepository extends CrudRepository<Users, Integer>{
     
 }
