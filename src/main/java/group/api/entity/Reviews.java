ff7cc2bd8b35e65a8076294995003d1cba267825
skydrying <ruvasikfarm1@mail.ru>
2025-11-19 23:37:28 +0700

    теперь полностью доделан телеграмм бот, поделан сайт

---
+++ src/main/java/group/api/entity/Reviews.java
@@ -0,0 +1,128 @@
+package group.api.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "reviews")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Reviews.findAll", query = "SELECT r FROM Reviews r"),
+    @NamedQuery(name = "Reviews.findById", query = "SELECT r FROM Reviews r WHERE r.id = :id"),
+    @NamedQuery(name = "Reviews.findByName", query = "SELECT r FROM Reviews r WHERE r.name = :name"),
+    @NamedQuery(name = "Reviews.findByEstimation", query = "SELECT r FROM Reviews r WHERE r.estimation = :estimation"),
+    @NamedQuery(name = "Reviews.findByDatereview", query = "SELECT r FROM Reviews r WHERE r.datereview = :datereview")})
+public class Reviews implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "estimation")
+    private int estimation;
+    @Column(name = "datereview")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datereview;
+    @JoinColumn(name = "id_customer", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Customer idCustomer;
+
+    public Reviews() {
+    }
+
+    public Reviews(Integer id) {
+        this.id = id;
+    }
+
+    public Reviews(Integer id, String name, int estimation) {
+        this.id = id;
+        this.name = name;
+        this.estimation = estimation;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEstimation() {
+        return estimation;
+    }
+
+    public void setEstimation(int estimation) {
+        this.estimation = estimation;
+    }
+
+    public Date getDatereview() {
+        return datereview;
+    }
+
+    public void setDatereview(Date datereview) {
+        this.datereview = datereview;
+    }
+
+    public Customer getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(Customer idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Reviews)) {
+            return false;
+        }
+        Reviews other = (Reviews) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "group.api.entity.Reviews[ id=" + id + " ]";
+    }
+    
+}
